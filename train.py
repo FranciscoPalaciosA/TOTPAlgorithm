de7760036e5ddc655605e575e5238f5b30418036
Alberto <alberttomarvel@gmail.com>
2021-03-30 19:35:25 -0600

    add train.py to train image recognition model

---
+++ train.py
@@ -0,0 +1,77 @@
+# import matplotlib.pyplot as plt
+# import pylab as pl
+# import cv2
+# import scipy
+
+# pl.gray()
+# Save image in set directory
+# Read RGB image
+# image = cv2.imread('./shape/Circle/w-x/-MWvOk5Ge6eV0ayub45k.png')
+# pl.matshow(img)
+# pl.show()
+# print(img)
+# scipy.misc.imread()
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# print(gray.shape)
+# cv2.imwrite('./shape/Circle/w-x/compressed/-MWvOk5Ge6eV0ayub45k.png', gray, [cv2.IMWRITE_PNG_COMPRESSION, 9])
+# image2 = cv2.imread('./shape/Circle/w-x/compressed/-MWvOk5Ge6eV0ayub45k.png')
+# print(image2.shape)
+# print(gray)
+
+import os
+import cv2
+import numpy as np
+import random
+from sklearn import ensemble
+
+movements = [
+        'Circle',
+        'Triangle',
+        'Infinity'
+        ]
+dataset = []
+labels = []
+for movement in movements:
+    dir = f'./shape/{movement}/w-z'
+    for filename in os.listdir(dir):
+        f = os.path.join(dir, filename)
+        if os.path.isfile(f):
+            img = cv2.imread(f)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            dataset.append(gray)
+            labels.append(movement)
+dataset = np.array(dataset)
+
+# for index, (image, label) in enumerate(dataset[:100]):
+#     print(image.shape, label)
+
+n_samples = len(dataset)
+x = dataset.reshape((n_samples, -1))
+y = labels
+
+sample_index = random.sample(range(len(x)), int((len(x)/5)*4))
+valid_index = [i for i in range(len(x)) if i not in sample_index]
+
+sample_images = [x[i] for i in sample_index]
+valid_images = [x[i] for i in valid_index]
+
+sample_target = [y[i] for i in sample_index]
+valid_target = [y[i] for i in valid_index]
+
+classifier = ensemble.RandomForestClassifier()
+
+classifier.fit(sample_images, sample_target)
+
+score = classifier.score(valid_images, valid_target)
+print(f'Score\t{str(score)}')
+
+test_inf = cv2.imread('./shape/Infinity/w-z/-MX-YxEfgqmEdfRAlTls.png')
+gray_inf = cv2.cvtColor(test_inf, cv2.COLOR_BGR2GRAY)
+
+# print(x[0].shape)
+# print(y[0])
+
+# print(classifier.predict(x[0].reshape(1,-1)))
+print(classifier.predict(gray_inf.reshape(1,-1)))
+print(classifier.predict_proba(gray_inf.reshape(1,-1)))
+print(classifier.classes_)
