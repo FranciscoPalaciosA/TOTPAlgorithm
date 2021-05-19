073e185804da70f36e1a8399519c2d5171cb87ce
Francisco Palacios A <34322522+FranciscoPalaciosA@users.noreply.github.com>
2021-05-19 18:57:26 -0500

    Improved

--- model/DLTrain.py
+++ model/DLTrain.py
@@ -14,8 +14,8 @@ USED_PAIR_OF_AXIS = 'x-z'
 #         'Diamond'
 #         ]
 
-img_height = 160
-img_width = 140
+img_height = 120
+img_width = 160
 batch_size = 64
 
 train_db = tf.keras.preprocessing.image_dataset_from_directory(
@@ -67,12 +67,11 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 model.summary()
 
-epochs=6
+epochs=1
 history = model.fit(
         train_db,
         validation_data=test_db,
         epochs=epochs,
-        
         )
 
 acc = history.history['accuracy']
@@ -82,24 +81,27 @@ val_loss = history.history['val_loss']
 
 epochs_range = range(epochs)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+# plt.figure(figsize=(8, 8))
+# plt.subplot(1, 2, 1)
+# plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
+# plt.subplot(1, 2, 2)
+# plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
 
-plt.show()
+# plt.show()
+
 
 model_yaml = model.to_yaml()
 with open("model.yaml", "w") as yaml_file:
     yaml_file.write(model_yaml)
 model.save_weights("model.h5")
 print("Saved model to disk")
+
+#model.save('CNNModel')
 
